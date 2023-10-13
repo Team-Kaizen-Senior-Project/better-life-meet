a3237307a9a2d596d8476c8edd9fbd54084a7431
Brandon Chu <brandoc318@gmail.com>
2023-10-13 14:07:14 -0700

    Create wireframe for pod view

---
+++ components/PodView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="max-w-[250px] border rounded-md p-2">
+    <div class="grid-cols-4 grid-rows-4 grid aspect-square">
+      <button
+        class="border rounded-md p-4 col-start-2 col-end-3 row-start-1 row-end-2"
+      ></button>
+      <button
+        class="border rounded-md p-4 col-start-3 col-end-4 row-start-1 row-end-2"
+      ></button>
+      <button
+        class="border rounded-md p-4 col-start-4 col-end-5 row-start-2 row-end-3"
+      ></button>
+      <button
+        class="border rounded-md p-4 col-start-4 col-end-5 row-start-3 row-end-4"
+      ></button>
+      <button
+        class="border rounded-md p-4 col-start-2 col-end-3 row-start-4 row-end-5"
+      ></button>
+      <button
+        class="border rounded-md p-4 col-start-3 col-end-4 row-start-4 row-end-5"
+      ></button>
+      <button
+        class="border rounded-md p-4 col-start-1 col-end-2 row-start-2 row-end-3"
+      ></button>
+      <button
+        class="border rounded-md p-4 col-start-1 col-end-2 row-start-3 row-end-4"
+      ></button>
+      <div
+        class="col-start-2 p-3 text-center bg-black text-white rounded-md shadow shadow-black shadow-lg col-end-4 row-start-2 row-end-4"
+      ></div>
+    </div>
+    <button class="rounded-md bg-blue-400 w-full text-white py-2 mt-4">
+      Request to join meeting
+    </button>
+  </div>
+</template>
---
+++ pages/pods/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="">
+    <div class="w-[90%] min-h-[88vh] max-w-[1500px] mx-auto">
+      <h1 class="text-4xl font-bold mt-4 mb-8">All Pods</h1>
+      <section class="xl:grid-cols-4 grid gap-4">
+        <PodView />
+        <PodView />
+        <PodView />
+        <PodView />
+        <PodView />
+        <PodView />
+        <PodView />
+        <PodView />
+      </section>
+    </div>
+  </main>
+</template>
