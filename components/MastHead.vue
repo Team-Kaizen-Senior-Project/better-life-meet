18ef21b088c524f2b64000243135dba0312f7fed
Brandon Chu <brandoc318@gmail.com>
2023-10-19 09:10:27 -0700

    finish pod outline & create placeholder header

--- components/MastHead.vue
+++ components/MastHead.vue
@@ -1,14 +1,27 @@
 <script setup></script>
 <template>
-  <section class="masthead">
-    <inner-column>
+  <header class="bg-black py-4">
+    <div
+      class="w-[90vw] max-w-[1600px] items-center mx-auto flex justify-between"
+    >
       <nav class="mast-head">
-        <NuxtLink class="logo" href="/">home</NuxtLink>
-        <br />
-        <NuxtLink to="/profile">Profile</NuxtLink>
+        <!-- TODO: add in logo svg -->
+        <NuxtLink href="/" class="flex gap-2">
+          <span class="font-semibold text-2xl text-blue-400">Tribe</span>
+          <span class="text-blue-400 text-2xl">Meet</span>
+        </NuxtLink>
       </nav>
-    </inner-column>
-  </section>
+      <div class="flex gap-2 items-center">
+        <button class="bg-white aspect-square text-lg h-[2.5rem] rounded-full">
+          U
+        </button>
+        <div class="flex flex-col text-white">
+          <p>User</p>
+          <p>user@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style scoped></style>
--- components/PodView.vue
+++ components/PodView.vue
@@ -2,28 +2,28 @@
   <div class="max-w-[250px] border rounded-md p-2">
     <div class="grid-cols-4 grid-rows-4 grid aspect-square">
       <button
-        class="border rounded-md p-4 col-start-2 col-end-3 row-start-1 row-end-2"
+        class="border rounded-md hover:bg-gray-300 p-4 col-start-2 col-end-3 row-start-1 row-end-2"
       ></button>
       <button
-        class="border rounded-md p-4 col-start-3 col-end-4 row-start-1 row-end-2"
+        class="border rounded-md p-4 hover:bg-gray-300 col-start-3 col-end-4 row-start-1 row-end-2"
       ></button>
       <button
-        class="border rounded-md p-4 col-start-4 col-end-5 row-start-2 row-end-3"
+        class="border rounded-md p-4 hover:bg-gray-300 col-start-4 col-end-5 row-start-2 row-end-3"
       ></button>
       <button
-        class="border rounded-md p-4 col-start-4 col-end-5 row-start-3 row-end-4"
+        class="border rounded-md p-4 hover:bg-gray-300 col-start-4 col-end-5 row-start-3 row-end-4"
       ></button>
       <button
-        class="border rounded-md p-4 col-start-2 col-end-3 row-start-4 row-end-5"
+        class="border rounded-md p-4 hover:bg-gray-300 col-start-2 col-end-3 row-start-4 row-end-5"
       ></button>
       <button
-        class="border rounded-md p-4 col-start-3 col-end-4 row-start-4 row-end-5"
+        class="border rounded-md p-4 hover:bg-gray-300 col-start-3 col-end-4 row-start-4 row-end-5"
       ></button>
       <button
-        class="border rounded-md p-4 col-start-1 col-end-2 row-start-2 row-end-3"
+        class="border rounded-md p-4 hover:bg-gray-300 col-start-1 col-end-2 row-start-2 row-end-3"
       ></button>
       <button
-        class="border rounded-md p-4 col-start-1 col-end-2 row-start-3 row-end-4"
+        class="border rounded-md p-4 hover:bg-gray-300 col-start-1 col-end-2 row-start-3 row-end-4"
       ></button>
       <div
         class="col-start-2 p-3 text-center bg-black text-white rounded-md shadow shadow-black shadow-lg col-end-4 row-start-2 row-end-4"
