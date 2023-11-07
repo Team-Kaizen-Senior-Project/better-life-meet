06e7bf55bd8407014c207503001982901014f6a5
Brian <bdlowery2@gmail.com>
2023-11-06 19:25:01 -0800

    Add more file paths for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,45 @@
+const animate = require('tailwindcss-animate')
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './components/**/*.{js,vue,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './app.vue',
-  ],
-  theme: {
-    extend: {
-      colors:{
-        'better-life-bg':"#1e293b"
-      }
-    },
-  },
-  plugins: [],
+module.exports = {
+	darkMode: ['class'],
+	content: [
+		`./components/**/*.{vue,js,ts}`,
+		`./layouts/**/*.vue`,
+		`./pages/**/*.vue`,
+		`./composables/**/*.{js,ts}`,
+		`./plugins/**/*.{js,ts}`,
+		`./utils/**/*.{js,ts}`,
+		`./App.{js,ts,vue}`,
+		`./app.{js,ts,vue}`,
+		`./Error.{js,ts,vue}`,
+		`./error.{js,ts,vue}`,
+		`./app.config.{js,ts}`,
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
+		extend: {
+			keyframes: {
+				'accordion-down': {
+					from: { height: 0 },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: 0 },
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+			},
+		},
+	},
+	plugins: [animate],
 }
