088f137bbf88324c3ecc99ad11cb66f4bc290326
Brian <bdlowery2@gmail.com>
2023-11-06 19:23:33 -0800

    Add vcalendar, clsx

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // ... other options
-  modules: ['@pinia/nuxt'],
-  css: ['~/assets/css/main.css'],
-  imports: {
-    dirs: ['./services'],
-  },
-  pinia: {
-    autoImports: ['defineStore', 'acceptHMRUpdate'],
-  },
-  devtools: {
-    enabled: true,
-  },
-  vue: {
-    compilerOptions: {
-      isCustomElement: (tag) =>
-        ['inner-column', 'module', 'text-content', 'input-field'].includes(tag),
-    },
-  },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+	// ... other options
+	modules: ['@pinia/nuxt', '@samk-dev/nuxt-vcalendar'],
+
+	css: ['~/assets/css/main.css'],
+	imports: {
+		dirs: ['./services'],
+	},
+	pinia: {
+		autoImports: ['defineStore', 'acceptHMRUpdate'],
+	},
+	devtools: {
+		enabled: true,
+	},
+	vue: {
+		compilerOptions: {
+			isCustomElement: (tag) => ['inner-column', 'module', 'text-content', 'input-field'].includes(tag),
+		},
+	},
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
 })
