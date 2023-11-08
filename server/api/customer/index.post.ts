934cd3de8b7a9f5092a8cf8ef37a9bc3c700776b
ddenaga <76412992+ddenaga@users.noreply.github.com>
2023-11-08 14:14:25 -0800

    Update Customer POST

--- server/api/customer/index.post.ts
+++ server/api/customer/index.post.ts
@@ -1,53 +1,34 @@
-import { Client, QuerySuccess, fql } from 'fauna'
+import { AbortError, Client, ServiceError, fql } from 'fauna'
 
-// Customer interface (read from POST request)
-interface Customer {
-  firstName: string
-  lastName: string
-  email: string
-  netWorth: string
-  [key: string]: any
-}
-
-// Endpoint for creating a customer
 export default defineEventHandler(async (event) => {
+  // Get Customer ID
+  const { id } = event.context.params as { id: string }
+  const body = await readBody(event)
+
   // Initialize Fauna client
   const client = new Client({
     secret: process.env.FAUNA_KEY,
   })
 
   try {
-    // Read in request body (use type assertion to validate body)
-    const customer: Customer = (await readBody(event)) as Customer
+    const query = fql`Customer.create(${body})`
+    const response = await client.query(query)
 
-    // Validate Body - Return Bad Request error
-    if (
-      !customer.firstName ||
-      !customer.lastName ||
-      !customer.email ||
-      !customer.netWorth
-    ) {
-      return createError({
-        statusCode: 400,
-        statusMessage: `Bad Request`,
+    return response
+  } catch (error: unknown) {
+    if (error instanceof AbortError) {
+      const abortError = error as AbortError
+      const abort = abortError.abort! as { message: string }
+      throw createError({
+        statusCode: abortError.httpStatus,
+        statusMessage: abort.message,
+      })
+    } else {
+      const serviceError = error as ServiceError
+      throw createError({
+        statusCode: serviceError.httpStatus,
+        statusMessage: serviceError.message,
       })
     }
-
-    // Create customer instance
-    const query = fql`Customer.create({${customer}})`
-    const document = await client.query(query)
-
-    // Return the updated document (meeting + attendies)
-    return document.data
-
-    // Catch error
-  } catch (error) {
-    console.log(error)
-
-    // Return Server error for all other errors
-    return createError({
-      statusCode: 500,
-      statusMessage: 'Internal Server Error',
-    })
   }
 })
