2fc80d20b86feb41187fb35160c5fae7a982f68b
Brian <bdlowery2@gmail.com>
2023-11-09 13:13:51 -0800

    add button instead of pic on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <img class="object-contain w-full h-full max-w-lg max-h-lg" src="./../public/sparsh.png" alt="Centered Image">
-  </div>
+	<div class="flex h-screen items-center justify-center text-white">
+		<Button class="text-3xl">
+			<NuxtLink to="/dashboard">Dashboard</NuxtLink>
+		</Button>
+	</div>
 </template>
