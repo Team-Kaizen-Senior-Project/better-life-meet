56f381276c44dc398ec576ee6849f0da1b25eb96
Brian <bdlowery2@gmail.com>
2023-11-08 20:10:57 -0800

    remove mediasoup

--- server/api/webrtc.ts
+++
@@ -1,8 +0,0 @@
-/*
-  This is the server-side logic for handling WebRTC signaling.
-*/
-import { defineEventHandler } from 'h3'
-
-export default defineEventHandler(async (event) => {
-  // TODO: server-side logic for handling initial signaling and SDP offer/answer
-})
--- services/webrtcService.js
+++
@@ -1,6 +0,0 @@
-export async function getWebRtcTransport() {
-    const transportParams = await useFetch('/api/webrtc-transport'); // temp name of the api
-    // use transportParams to construct and configure an RTCPeerConnection on the client side
-    return transportParams;
-  }
-  
