a1614f0ec03caeaa59759119ef315d0be46496d2
Brian <bdlowery2@gmail.com>
2023-11-09 13:07:19 -0800

    Fix HMR being slow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // ... other options
-  modules: ['@pinia/nuxt', '@nuxt/ui'],
-  css: ['~/assets/css/main.css'],
-  imports: {
-    dirs: ['./services'],
-  },
-  // @ts-ignore
-  colorMode: {
-    preference: 'light',
-  },
-  pinia: {
-    autoImports: ['defineStore', 'acceptHMRUpdate'],
-  },
-  devtools: {
-    enabled: true,
-  },
-  vue: {
-    compilerOptions: {
-      isCustomElement: (tag) =>
-        ['inner-column', 'module', 'text-content', 'input-field'].includes(tag),
-    },
-  },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+	// ... other options
+	modules: ['@pinia/nuxt', '@nuxt/ui', '@samk-dev/nuxt-vcalendar'],
+	// css: ['~/assets/css/main.css'],
+	tailwindcss: {
+		cssPath: '~/assets/css/main.css',
+	},
+	imports: {
+		dirs: ['./services'],
+	},
+	// @ts-ignore
+	colorMode: {
+		preference: 'light',
+	},
+	pinia: {
+		autoImports: ['defineStore', 'acceptHMRUpdate'],
+	},
+	devtools: {
+		enabled: true,
+	},
+	vue: {
+		compilerOptions: {
+			isCustomElement: (tag) => ['inner-column', 'module', 'text-content', 'input-field'].includes(tag),
+		},
+	},
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
 })
