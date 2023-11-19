ae6e00e0fec45ac8de1c222587b5cc911aae95b6
ddenaga <76412992+ddenaga@users.noreply.github.com>
2023-11-19 15:02:24 -0800

    Initial setup of mediasoup server

---
+++ mediasoup-server/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import type { Request, Response } from 'express'
+
+import mediasoup from 'mediasoup'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello, world!')
+})
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
