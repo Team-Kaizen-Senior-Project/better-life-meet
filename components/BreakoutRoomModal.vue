f7d93e0672e7249514933ed089f3c9aead942f66
Brandon Chu <brandoc318@gmail.com>
2023-11-21 23:37:39 -0800

    KAIZ-45
    KAIZ-45

---
+++ components/BreakoutRoomModal.vue
@@ -0,0 +1,111 @@
+<script setup>
+	import { Cog6ToothIcon } from '@heroicons/vue/24/outline'
+	import { VideoCameraSlashIcon } from '@heroicons/vue/24/outline'
+	import { VideoCameraIcon } from '@heroicons/vue/24/outline'
+	import { MicrophoneIcon } from '@heroicons/vue/24/solid'
+	import { ref } from 'vue'
+
+	const modalIsOpen = ref(false)
+	const isCameraOn = ref(false)
+	const videoPreview = ref(null)
+
+	const customModal = ref({
+		overlay: {
+			background: 'bg-zinc-900/90 dark:bg-gray-800/75',
+		},
+	})
+
+	function startUp() {
+		const videoElement = videoPreview.value
+		// Handle permissions for camera if needed
+		navigator.mediaDevices
+			.getUserMedia({ video: true, audio: false })
+			.then(function (stream) {
+				if (!isCameraOn.value) {
+					videoElement.classList.remove('hidden')
+					videoElement.srcObject = stream
+					videoElement.play()
+				} else {
+					stopRecording()
+				}
+			})
+			.catch(function (err) {
+				console.error(err)
+			})
+
+		videoElement.addEventListener(
+			'canplay',
+			function (ev) {
+				isCameraOn.value = true
+			},
+			false,
+		)
+	}
+	function stopRecording() {
+		videoPreview.value.classList.add('hidden')
+		videoPreview.value.srcObject.getTracks().forEach((track) => track.stop())
+		isCameraOn.value = false
+	}
+	function joinMeeting() {
+		const videoElement = document.getElementById('demo-video-element')
+		modalIsOpen.value = false
+		navigator.mediaDevices
+			.getUserMedia({ video: true, audio: false })
+			.then(function (stream) {
+				if (isCameraOn.value) {
+					videoElement.srcObject = stream
+					videoElement.play()
+				}
+			})
+			.catch(function (err) {
+				console.error(err)
+			})
+		videoElement.addEventListener(
+			'canplay',
+			function (ev) {
+				isCameraOn.value = true
+			},
+			false,
+		)
+	}
+	onMounted(() => {
+		// Setting this to true in the ref initially breaks the close modal
+		modalIsOpen.value = true
+	})
+</script>
+<template>
+	<UModal v-model="modalIsOpen" class="w-[90vw] max-w-[400px]" :ui="customModal">
+		<div class="rounded bg-zinc-800 p-4 shadow-lg">
+			<div class="mb-2">
+				<p class="text-lg font-semibold text-white">Pod accountability meeting is starting</p>
+				<p class="text-gray-200">
+					This is your chance to make sure your camera is setup and your microphone is working
+				</p>
+			</div>
+			<div class="flex h-[360px] w-full items-center justify-center rounded-md border border-dashed">
+				<div v-if="isCameraOn === false" class="h-[4rem] w-[4rem] text-white"><VideoCameraSlashIcon /></div>
+				<video ref="videoPreview" class="hidden rounded-md"></video>
+			</div>
+			<div class="mt-2 flex flex-col gap-2">
+				<MicControls box-length="50" />
+				<div class="flex justify-between">
+					<div class="flex gap-2">
+						<Button class="h-10 w-10"><Cog6ToothIcon /></Button>
+						<Button @click="startUp" class="h-10 w-10">
+							<VideoCameraSlashIcon v-if="isCameraOn === false" />
+							<VideoCameraIcon v-if="isCameraOn === true" />
+						</Button>
+						<Button class="h-10 w-10"><MicrophoneIcon /></Button>
+					</div>
+					<button
+						type="button"
+						@click="joinMeeting"
+						class="rounded-md bg-sky-500 px-3 py-1.5 font-medium text-white hover:bg-sky-600"
+					>
+						Join
+					</button>
+				</div>
+			</div>
+		</div>
+	</UModal>
+</template>
---
+++ pages/meeting/index.vue
@@ -0,0 +1,44 @@
+<script setup>
+	definePageMeta({
+		layout: false,
+	})
+</script>
+
+<template>
+	<main class="h-[100vh] bg-zinc-800">
+		<PodHeader />
+		<div class="flex min-h-[82vh] items-center justify-center">
+			<div class="grid h-[70vh] w-[80vw] grid-cols-4 grid-rows-2 gap-3">
+				<!-- TODO: refactor with actual video outputs -->
+				<div class="relative border border-dashed bg-zinc-700">
+					<!-- FOR DEMO PURPOSES -->
+					<video id="demo-video-element" class="h-full w-full"></video>
+					<p class="absolute bottom-0 left-0 bg-black px-2 py-1.5 text-white">Matt</p>
+				</div>
+				<div class="relative border border-dashed bg-zinc-700">
+					<p class="absolute bottom-0 left-0 bg-black px-2 py-1.5 text-white">Brandon</p>
+				</div>
+				<div class="relative border border-dashed bg-zinc-700">
+					<p class="absolute bottom-0 left-0 bg-black px-2 py-1.5 text-white">Victor</p>
+				</div>
+				<div class="relative border border-dashed bg-zinc-700">
+					<p class="absolute bottom-0 left-0 bg-black px-2 py-1.5 text-white">Fabian</p>
+				</div>
+				<div class="relative border border-dashed bg-zinc-700">
+					<p class="absolute bottom-0 left-0 bg-black px-2 py-1.5 text-white">Sparsh</p>
+				</div>
+				<div class="relative border border-dashed bg-zinc-700">
+					<p class="absolute bottom-0 left-0 bg-black px-2 py-1.5 text-white">Jasjot</p>
+				</div>
+				<div class="relative border border-dashed bg-zinc-700">
+					<p class="absolute bottom-0 left-0 bg-black px-2 py-1.5 text-white">Brian</p>
+				</div>
+				<div class="relative border border-dashed bg-zinc-700">
+					<p class="absolute bottom-0 left-0 bg-black px-2 py-1.5 text-white">Daryl</p>
+				</div>
+			</div>
+		</div>
+		<BreakoutRoomModal />
+		<PodFooter />
+	</main>
+</template>
