caba996c7c0e43a9586c14d532f8717266d1f01c
Brian <bdlowery2@gmail.com>
2023-11-25 13:12:43 -0800

    Make habits look cleaner

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,50 +1,50 @@
 <script setup>
-import { Cog6ToothIcon } from '@heroicons/vue/24/outline'
-import { VideoCameraSlashIcon } from '@heroicons/vue/24/outline'
-import { MicrophoneIcon } from '@heroicons/vue/24/solid'
-import MeetingCard from '@/components/MeetingCard.vue'
-const meetings = [
-	{
-		title: 'Week 16 Accountability Meeting',
-		date: 'Wednesday October 4th',
-		time: '4:00pm',
-		link: '#link1',
-		isLive: true,
-	},
-	{
-		title: 'Week 17 Accountability Meeting',
-		date: 'Wednesday October 11th',
-		time: '4:00pm',
-		link: '#link2',
-		isLive: false,
-	},
-	{
-		title: 'Week 18 Accountability Meeting',
-		date: 'Friday, October 20th',
-		time: '2:00pm',
-		link: '#link3',
-		isLive: false,
-	},
-	{
-		title: 'Week 19 Accountability Meeting',
-		date: 'Thursday October 26th',
-		time: '3:00 PM',
-		link: '#link4',
-		isLive: false,
-	},
-]
-const date = ref(new Date())
-
-const attrs = ref([
-	{
-		key: 'today',
-		highlight: {
-			color: 'green',
-			fillMode: 'solid',
+	import { Cog6ToothIcon } from '@heroicons/vue/24/outline'
+	import { VideoCameraSlashIcon } from '@heroicons/vue/24/outline'
+	import { MicrophoneIcon } from '@heroicons/vue/24/solid'
+	import MeetingCard from '@/components/MeetingCard.vue'
+	const meetings = [
+		{
+			title: 'Week 16 Accountability Meeting',
+			date: 'Wednesday October 4th',
+			time: '4:00pm',
+			link: '#link1',
+			isLive: true,
 		},
-		dates: new Date(),
-	},
-])
+		{
+			title: 'Week 17 Accountability Meeting',
+			date: 'Wednesday October 11th',
+			time: '4:00pm',
+			link: '#link2',
+			isLive: false,
+		},
+		{
+			title: 'Week 18 Accountability Meeting',
+			date: 'Friday, October 20th',
+			time: '2:00pm',
+			link: '#link3',
+			isLive: false,
+		},
+		{
+			title: 'Week 19 Accountability Meeting',
+			date: 'Thursday October 26th',
+			time: '3:00 PM',
+			link: '#link4',
+			isLive: false,
+		},
+	]
+	const date = ref(new Date())
+
+	const attrs = ref([
+		{
+			key: 'today',
+			highlight: {
+				color: 'green',
+				fillMode: 'solid',
+			},
+			dates: new Date(),
+		},
+	])
 </script>
 
 <template>
@@ -56,38 +56,14 @@ const attrs = ref([
 					<HabitsList />
 					<div class="border border-dashed border-zinc-500 p-4">
 						<h2 class="mb-4 text-lg font-medium">Your goals for the quarter</h2>
-						<ul class="grid gap-2">
-							<li v-for="i in 3" :key="i">Lorem ipsum dolor sit amet consectetur</li>
+						<ul class="grid gap-2 pl-4">
+							<li v-for="i in 3" :key="i" class="list-disc text-sm">Lorem ipsum dolor sit amet consectetur</li>
 						</ul>
 					</div>
 				</div>
 
 				<!-- column 2 -->
 				<div class="col-span-1 grid gap-4 md:col-span-6">
-					<!-- <div class="border border-dashed border-zinc-500 p-4">
-						<h2>Video settings</h2>
-						<div>
-							<picture class="max-w-md border border-dashed border-zinc-500">
-								<img
-									src="https://ucarecdn.com/46acb38a-3e2e-40d9-9bbf-14e79b6293eb/CleanShot20231106at1652362x.png"
-									alt=""
-								/>
-							</picture>
-							<div class="mt-2 flex flex-row gap-2">
-								<Button type="primary">
-									<div class="h-4 w-4">
-										<Cog6ToothIcon />
-									</div>
-								</Button>
-								<Button type="primary" disabled>
-									<div class="h-4 w-4"><VideoCameraSlashIcon /></div>
-								</Button>
-								<Button type="primary">
-									<div class="h-4 w-4"><MicrophoneIcon /></div>
-								</Button>
-							</div>
-						</div>
-					</div> -->
 					<VideoSettings />
 
 					<div class="grid grid-cols-2 gap-4 lg:grid-cols-2">
@@ -110,13 +86,13 @@ const attrs = ref([
 	</section>
 </template>
 <style scoped>
-.count {
-	display: flex;
-	gap: 10px;
-	align-items: flex-start;
-}
+	.count {
+		display: flex;
+		gap: 10px;
+		align-items: flex-start;
+	}
 
-h3 {
-	margin: 0;
-}
+	h3 {
+		margin: 0;
+	}
 </style>
