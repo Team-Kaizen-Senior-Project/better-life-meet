aad115f27586aa5a8bf5a6026beb8dccc87fb80a
Matt Christiansen <mattchristiansen59@gmail.com>
2023-11-25 15:44:34 -0800

    setup additions

--- mediasoup-server/src/server.ts
+++ mediasoup-server/src/server.ts
@@ -1,21 +1,26 @@
 import express from 'express'
 import type { Request, Response } from 'express'
-
+import { Server as SocketIOServer } from 'socket.io'
 import * as mediasoup from 'mediasoup'
+import http from 'http'
+import { types as mediasoupTypes } from "mediasoup";
+
 
 const app = express()
 const port = process.env.PORT || 3000
+const httpServer = http.createServer(app)
+const io = new SocketIOServer(httpServer)
 
-let worker;
-let router;
-let transport;
+let worker: mediasoupTypes.Worker
+let router: mediasoupTypes.Router
+let transport: mediasoupTypes.WebRtcTransport
+
 
 async function startMediaSoup() {
 	try {
 		worker = await mediasoup.createWorker({
 			logLevel: 'warn',
-
-		});
+		})
 		// router with RTP capabilities
 		router = await worker.createRouter({
 			mediaCodecs: [
@@ -34,7 +39,7 @@ async function startMediaSoup() {
 					},
 				},
 			],
-		});
+		})
 		// create transport
 		transport = await router.createWebRtcTransport({
 			listenIps: [{ ip: '127.0.0.1'}],
@@ -42,15 +47,15 @@ async function startMediaSoup() {
 			enableTcp: true,
 			preferUdp: true,
 		});
-		console.log('mediasoup started successfully');
+		console.log('mediasoup started successfully')
+		console.log("mediasoup version: " + mediasoup.version)
 	} catch (err) {	
-		console.error('Error starting mediasoup:', err);
+		console.error('Error starting mediasoup:', err)
 	}
 }
 
 // Initialize mediasoup 
-startMediaSoup();
-
+startMediaSoup()
 
 // Express route handlers
 app.get('/', (req: Request, res: Response) => {
