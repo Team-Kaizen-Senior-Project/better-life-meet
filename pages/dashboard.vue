65997769eedf28a4aa05524b87e1a7b9b468f43f
Brian <bdlowery2@gmail.com>
2023-11-25 15:59:04 -0800

    Move upcoming meetings under calendar

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -11,27 +11,6 @@
 			link: '#link1',
 			isLive: true,
 		},
-		{
-			title: 'Week 17 Accountability Meeting',
-			date: 'Wednesday October 11th',
-			time: '4:00pm',
-			link: '#link2',
-			isLive: false,
-		},
-		{
-			title: 'Week 18 Accountability Meeting',
-			date: 'Friday, October 20th',
-			time: '2:00pm',
-			link: '#link3',
-			isLive: false,
-		},
-		{
-			title: 'Week 19 Accountability Meeting',
-			date: 'Thursday October 26th',
-			time: '3:00 PM',
-			link: '#link4',
-			isLive: false,
-		},
 	]
 	const date = ref(new Date())
 
@@ -54,7 +33,7 @@
 				<!-- column 1 -->
 				<div class="col-span-1 grid gap-4 self-start lg:col-span-3">
 					<HabitsList />
-					<div class="border border-dashed border-zinc-500 p-4">
+					<div class="rounded-lg bg-zinc-900 p-4">
 						<h2 class="mb-4 text-lg font-medium">Your goals for the quarter</h2>
 						<ul class="grid gap-2 pl-4">
 							<li v-for="i in 3" :key="i" class="list-disc text-sm">Lorem ipsum dolor sit amet consectetur</li>
@@ -65,21 +44,27 @@
 				<!-- column 2 -->
 				<div class="col-span-1 grid gap-4 md:col-span-6">
 					<VideoSettings />
-
-					<div class="grid grid-cols-2 gap-4 lg:grid-cols-2">
-						<div v-for="meeting in meetings" :key="meeting.title" class="">
-							<MeetingCard :meeting="meeting" :isLive="meeting.isLive" />
-						</div>
-					</div>
 				</div>
 				<!-- column 3 -->
-				<div class="col-span-1 lg:col-span-3">
-					<div class="mb-4">
+				<div class="col-span-1 grid gap-4 self-start lg:col-span-3">
+					<div class="rounded-lg bg-zinc-900 p-4">
 						<h2 class="mb-4 text-lg font-medium text-white">Upcoming events</h2>
-						<VCalendar v-model="date" :attributes="attrs" isDark="true" />
+						<div class="my-calendar">
+							<VCalendar v-model="date" :attributes="attrs" class="" isDark />
+						</div>
+						<div class="mt-4">
+							<ScheduleMeetingCard />
+						</div>
 					</div>
 
-					<ScheduleMeetingCard />
+					<div class="rounded-lg bg-zinc-900 p-4">
+						<h2 class="mb-4 text-lg font-medium text-white">Meetings for today</h2>
+						<div class="grid grid-cols-1 gap-4">
+							<div v-for="meeting in meetings" :key="meeting.title" class="">
+								<MeetingCard :meeting="meeting" :isLive="meeting.isLive" />
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</inner-column>
@@ -90,6 +75,13 @@
 		display: flex;
 		gap: 10px;
 		align-items: flex-start;
+	}
+
+	.my-calendar :deep(.vc-dark) {
+		--vc-bg: #27272a;
+	}
+	.my-calendar :deep(.vc-bordered) {
+		border-color: transparent;
 	}
 
 	h3 {
