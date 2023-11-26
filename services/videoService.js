a0a9fa21734d971523c613a9c3786d10c51498f0
Brian <bdlowery2@gmail.com>
2023-11-25 20:37:48 -0800

    Add videoService store

---
+++ services/videoService.js
@@ -0,0 +1,23 @@
+export const useVideoStore = defineStore('video', () => {
+	const cameraActive = ref(false)
+	const joinedMeeting = ref(false)
+	const modalOpen = ref(false)
+
+	const toggleCamera = () => {
+		cameraActive.value = !cameraActive.value
+	}
+
+	const joinMeeting = () => {
+		joinedMeeting.value = true
+		modalOpen.value = !modalOpen.value
+	}
+
+	const leaveMeeting = () => {
+		joinedMeeting.value = false
+		modalOpen.value = false
+		if (cameraActive.value) {
+			toggleCamera()
+		}
+	}
+	return { cameraActive, toggleCamera, joinedMeeting, joinMeeting, leaveMeeting, modalOpen }
+})
