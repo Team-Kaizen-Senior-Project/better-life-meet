c252c8e4340c9384033569173df2108a28048dde
Brian <bdlowery2@gmail.com>
2023-11-25 16:00:37 -0800

    Add dark bg to metting cards, adjust font styles

--- components/MeetingCard.vue
+++ components/MeetingCard.vue
@@ -1,29 +1,4 @@
-<template>
-	<div class="border border-dashed p-3" :class="borderClass">
-		<div class="grid gap-2">
-			<div class="text-md font-medium text-white">{{ meeting.title }}</div>
-			<div class="grid gap-1">
-				<p class="text-sm text-zinc-300">{{ meeting.date }}</p>
-				<p class="text-sm text-zinc-300">{{ meeting.time }}</p>
-			</div>
-		</div>
-		<div class="flex w-full items-center justify-between">
-			<div></div>
-			<!-- Spacer -->
-			<a
-				:href="meeting.link"
-				class="flex h-8 gap-2 rounded-md px-3 py-2 font-['Inter'] text-sm font-medium leading-tight text-white shadow"
-				:class="buttonClass"
-			>
-				Join
-			</a>
-		</div>
-	</div>
-</template>
-
 <script setup>
-	import { computed } from 'vue'
-
 	const props = defineProps({
 		meeting: Object,
 		isLive: Boolean,
@@ -32,3 +7,24 @@
 	const borderClass = computed(() => (props.isLive ? 'border-emerald-600' : 'border-white'))
 	const buttonClass = computed(() => (props.isLive ? 'bg-sky-500 hover:bg-sky-600' : 'bg-zinc-700'))
 </script>
+
+<template>
+	<div class="rounded-lg bg-zinc-800 p-4" :class="borderClass">
+		<div class="grid gap-2">
+			<div class="text-sm font-medium text-white">{{ meeting.title }}</div>
+			<div class="grid gap-1">
+				<p class="text-sm text-zinc-300">{{ meeting.date }}</p>
+				<p class="text-sm text-zinc-300">{{ meeting.time }}</p>
+			</div>
+		</div>
+		<div class="flex w-full items-center justify-between">
+			<a
+				:href="meeting.link"
+				class="mt-4 flex gap-2 rounded-md px-3 py-2 text-sm font-medium leading-tight text-white shadow"
+				:class="buttonClass"
+			>
+				Join
+			</a>
+		</div>
+	</div>
+</template>
