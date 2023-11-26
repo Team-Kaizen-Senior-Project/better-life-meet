da4bf6c7d6c9a91ea23eb78c60b92b95a4938469
Sparsh <65922841+sparshs916@users.noreply.github.com>
2023-11-26 15:34:29 -0800

    Added Pod Redirect Page UI (#20)

---
+++ components/Pod.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="w-[304px] h-[338px] pl-[11px] pr-3 pt-5 pb-3 bg-white rounded-lg border border-neutral-300 flex-col justify-end items-center gap-8 inline-flex">
+    <div class="w-[234px] h-[234px] relative">
+        <div class="w-[108px] h-[50px] left-[63px] top-[184px] absolute justify-start items-start gap-2 inline-flex">
+            <div class="w-[50px] h-[50px] bg-slate-50 rounded-lg border border-neutral-400" />
+            <div class="w-[50px] h-[50px] bg-slate-50 rounded-lg border border-neutral-400" />
+        </div>
+        <div class="w-[108px] h-[50px] left-[63px] top-0 absolute justify-start items-start gap-2 inline-flex">
+            <img class="w-[50px] h-[50px] rounded-lg border border-neutral-400"  />
+            <div class="w-[50px] h-[50px] bg-slate-50 rounded-lg border border-neutral-400" />
+        </div>
+        <div class="w-[50px] h-[108px] left-0 top-[63px] absolute flex-col justify-start items-start gap-2 inline-flex">
+            <div class="w-[50px] h-[50px] bg-slate-50 rounded-lg border border-neutral-400" />
+            <div class="w-[50px] h-[50px] bg-slate-50 rounded-lg border border-neutral-400" />
+        </div>
+        <div class="w-[50px] h-[108px] left-[184px] top-[63px] absolute flex-col justify-start items-start gap-2 inline-flex">
+            <div class="w-[50px] h-[50px] bg-slate-50 rounded-lg border border-neutral-400" />
+            <div class="w-[50px] h-[50px] bg-slate-50 rounded-lg border border-neutral-400" />
+        </div>
+        <div class="w-[150px] h-[150px] left-[42px] top-[42px] absolute">
+            <div class="w-[150px] h-[150px] left-0 top-0 absolute bg-black rounded-lg shadow" />
+        </div>
+    </div>
+    <div class="cursor-pointer w-[281px] h-10 px-4 py-2 bg-blue-500 hover:bg-blue-700 rounded-md shadow justify-center items-center gap-2 inline-flex">
+        <div class="text-white text-sm font-normal font-['Inter'] leading-tight">Join pod meeting</div>
+    </div>
+</div>
+</template>
---
+++ pages/redirect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="min-h-screen  border-zinc-500 bg-zinc-800"> <!-- Ensures full height of screen and sets a light background -->
+    <h1 class="text-5xl font-bold text-left pl-20 pt-10 mb-10 text-white">All Pods</h1> <!-- Large, bold text with padding -->
+    <div class="flex flex-wrap justify-center gap-4 m-4">
+      <LoneRangerPod/>
+      <Pod/>
+      <Pod/>
+      <Pod/>
+      <Pod/>
+      <Pod/>
+      <Pod/>
+      <Pod/>
+      <Pod/>
+      <Pod/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+/* Additional styles if necessary */
+</style>
