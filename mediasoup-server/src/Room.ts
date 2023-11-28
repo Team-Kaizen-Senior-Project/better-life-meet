7098ac79366d024278df4f44612a43e07f402b1c
Matt Christiansen <mattchristiansen59@gmail.com>
2023-11-27 18:41:37 -0800

    KAIZ-78 (#21)
    * Initial setup of mediasoup server
    
    * initalization of worker, router, and transport
    
    * setup additions
    
    * updates for producer and consumer additions
    
    * updated server to work with config.ts
    
    * added Peer class for managing state of each peer
    
    * cleaned up Peer
    
    * cleaned up Room
    
    * cleaned config
    
    * cleaned Room
    
    * cleaned up Room and server
    
    * cleaned
    
    * cleaned
    
    * cleaned server
    
    ---------
    
    Co-authored-by: ddenaga <76412992+ddenaga@users.noreply.github.com>

---
+++ mediasoup-server/src/Room.ts
@@ -0,0 +1,139 @@
+import config from './config'
+import { types as mediasoupTypes } from "mediasoup"
+import { Server as SocketIOServer } from 'socket.io'
+import Peer from './Peer'
+
+interface Producer {
+    id: string
+}
+
+class Room {
+    id: string
+    router: mediasoupTypes.Router | undefined
+    io: SocketIOServer
+    peers: Map<string, Peer>
+  
+    constructor(room_id: string, worker: mediasoupTypes.Worker, io: SocketIOServer) {
+      this.id = room_id
+      this.io = io
+      this.peers = new Map<string, Peer>()
+      this.initializeRouter(worker)
+    }
+  
+    async initializeRouter(worker: mediasoupTypes.Worker) {
+      const mediaCodecs = config.mediasoup.router.mediaCodecs
+  
+      try {
+        const router = await worker.createRouter({ mediaCodecs })
+        this.router = router
+      } catch (error) {
+        console.error('Error creating router:', error)
+      }
+    }
+
+    addPeer(peer: Peer) {
+        this.peers.set(peer.id, peer)
+    }
+
+    getProducerListForPeer(): Array<{ producer_id: string }> {
+        let producerList: Array<{ producer_id: string }> = []
+        this.peers.forEach(peer => {
+            peer.producers.forEach((producer: Producer) => {
+                producerList.push({ producer_id: producer.id })
+            });
+        });
+        return producerList
+    }
+
+    getRtpCapabilities() {
+        if (!this.router) {
+            console.error('Router is not initialized yet')
+            return
+        }
+        return this.router.rtpCapabilities
+    }
+
+    async connectPeerTransport(socket_id: string, transport: mediasoupTypes.WebRtcTransport, 
+        dtlsParameters: mediasoupTypes.DtlsParameters) {
+        if (!this.router) {
+            console.error('Router is not initialized yet, cannot connect peer transport')
+            return
+        }
+        const peer = this.peers.get(socket_id)
+        if (!peer) {
+            console.error('Peer not found')
+            throw new Error('Peer not found')
+        }
+        await peer.connectTransport(transport.id, dtlsParameters)
+    }
+
+    // allow a peer in room to create and send a media stream (producer)
+    async produce(socket_id: string, producerTransportId: string, rtpParameters: mediasoupTypes.RtpParameters, kind: mediasoupTypes.MediaKind): Promise<any> {
+        if (!this.router) {
+            console.error('Router is not initialized yet, cannot make producer')
+            throw new Error('Router is not initialized')
+        }
+        const peer = this.peers.get(socket_id)
+        if (!peer) {
+            console.error('Peer not found')
+            throw new Error('Peer not found')
+        }
+        let producer = await peer.createProducer(producerTransportId, rtpParameters, kind)
+        
+        // broadcasting new producer to other peers in the room
+        this.broadCast(socket_id, 'newProducers', [{
+            producer_id: producer.id,
+            producer_socket_id: socket_id
+        }]);
+
+        return producer.id 
+    }
+    
+    // Create a consumer for specific peer in the room
+    async consume(socket_id: string, consumer_transport_id: string, producer_id: string, rtpCapabilities: mediasoupTypes.RtpCapabilities): Promise<any> { 
+        if (!this.router || !this.router.canConsume({
+            producerId: producer_id,
+            rtpCapabilities,
+        })) {
+            console.error('Cannot consume: router not initialized or cannot consume')
+            throw new Error('Cannot consume')
+        }
+        const peer = this.peers.get(socket_id)
+        if (!peer) {
+            console.error('Peer not found')
+            throw new Error('Peer not found')
+        }
+
+        let { consumer, params } = await peer.createConsumer(consumer_transport_id, producer_id, rtpCapabilities)
+
+        consumer.on('producerclose', () => {
+            console.log('Consumer closed due to producerclose event', {
+                name: `${this.peers.get(socket_id)?.name}`,
+                consumer_id: consumer.id,
+            });
+            this.peers.get(socket_id)?.removeConsumer(consumer.id);
+            this.io.to(socket_id).emit('consumerClosed', {
+                consumer_id: consumer.id,
+            })
+        })
+
+        return params
+    }
+
+    // Broadcast a message to all peers except the one specified by socket_id
+    broadCast(socket_id: string, name: string, data: any): void {
+        for (let otherID of Array.from(this.peers.keys()).filter(id => id !== socket_id)) {
+            this.send(otherID, name, data)
+        }
+    } 
+    
+    send(socket_id: string, name: string, data: any): void {
+        this.io.to(socket_id).emit(name, data)
+    }
+
+    getPeers(): Map<string, Peer> {
+        return this.peers
+    }
+}
+
+export default Room
---
+++ mediasoup-server/src/server.ts
@@ -0,0 +1,83 @@
+import express from 'express'
+import type { Request, Response } from 'express'
+import { Server as SocketIOServer } from 'socket.io'
+import * as mediasoup from 'mediasoup'
+import http from 'http'
+import { types as mediasoupTypes } from 'mediasoup'
+import Room from './Room'
+import config from './config'
+
+const app = express()
+const port = process.env.PORT || 3000
+const httpServer = http.createServer(app)
+const io = new SocketIOServer(httpServer)
+const mediaCodecs = config.mediasoup.router.mediaCodecs
+
+let worker: mediasoupTypes.Worker
+let router: mediasoupTypes.Router
+let transport: mediasoupTypes.WebRtcTransport
+let roomList = new Map()
+
+async function startMediaSoup() {
+	try {
+		worker = await mediasoup.createWorker({
+			logLevel: 'warn',
+		})
+		// router with RTP capabilities
+		router = await worker.createRouter({
+			mediaCodecs: mediaCodecs,
+		})
+		// create transport
+		transport = await router.createWebRtcTransport({
+			listenIps: [{ ip: '127.0.0.1'}],
+			enableUdp: true,
+			enableTcp: true,
+			preferUdp: true,
+		});
+		console.log('mediasoup started successfully')
+		console.log("mediasoup version: " + mediasoup.version)
+	} catch (err) {	
+		console.error('Error starting mediasoup:', err)
+	}
+}
+
+// Initialize mediasoup 
+startMediaSoup()
+
+// Express route handlers
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello, world!')
+})
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
+
+function getMediasoupWorker() {
+	if (worker) {
+		return worker
+	}
+	else console.error('Mediasoup worker not initialized')
+}
+
+// event listener to handle socket connections
+io.on('connection', (socket) => {
+	socket.on('createRoom', async ({ room_id }, callback) => {
+		if (roomList.has(room_id)) {
+			callback('already exists')
+		} else {
+			console.log('Created room', { room_id: room_id })
+
+			let worker = getMediasoupWorker()
+			if (!worker) {
+				console.error('Failed to get Mediasoup worker')
+				callback('Server error')
+				return
+			}
+			
+			roomList.set(room_id, new Room(room_id, worker, io))
+			callback(room_id)
+		}
+	})
+})
+// TODO: FINISH ME
