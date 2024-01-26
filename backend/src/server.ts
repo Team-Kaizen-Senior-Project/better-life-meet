8d110a165830d04ad4e71d73f35b8e0e28a53016
ddenaga <76412992+ddenaga@users.noreply.github.com>
2024-01-26 12:41:41 -0800

    Set up mediasoup backend

---
+++ backend/src/server.ts
@@ -0,0 +1,51 @@
+import http from 'http'
+import io from 'socket.io'
+import express, { Express } from 'express'
+import cors from 'cors'
+
+const initializeExpressServer = (): Express => {
+	const app: Express = express()
+	app.use(cors())
+
+	// * Specify Express endpoints here
+	app.get('/mediasoup', (req, res) => {
+		res.send('Hello, world!')
+	})
+
+	return app
+}
+
+const initializeWebSocketServer = (httpServer: http.Server): io.Server => {
+	const wss: io.Server = new io.Server(httpServer, {
+		path: '/wss',
+		cors: {
+			origin: '*',
+		},
+	})
+
+	// * Specify WebSocket events here
+	wss.on('connection', async (socket: io.Socket) => {
+		socket.on('greet', (data) => {
+			console.log(data)
+			socket.emit('Hello, world!')
+		})
+	})
+
+	return wss
+}
+
+const main = async () => {
+	const app: Express = initializeExpressServer()
+
+	const httpServer: http.Server = new http.Server(app)
+
+	const wss: io.Server = initializeWebSocketServer(httpServer)
+
+	httpServer.listen(3333, () => {
+		console.log('Listening on port 3333')
+	})
+}
+
+if (require.main === module) {
+	main()
+}
