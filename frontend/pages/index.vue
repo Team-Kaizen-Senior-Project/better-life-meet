d9f72f5791a1126316a3f4338fad91925c5f4a1d
ddenaga <76412992+ddenaga@users.noreply.github.com>
2024-01-26 16:54:15 -0800

    Add python3-pip dependency

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,3 +5,17 @@
 		</Button>
 	</div>
 </template>
+
+<script setup lang="ts">
+	// import { io } from 'socket.io-client'
+	// const ws = io({ path: '/wss' })
+	// ws.emit("greet", "Hello, world!");
+
+	// import { detectDevice } from 'mediasoup-client'
+	// const handlerName = detectDevice()
+	// if (handlerName) {
+	// 	console.log('detected handler: %s', handlerName)
+	// } else {
+	// 	console.warn('no suitable handler found for current browser/device')
+	// }
+</script>
