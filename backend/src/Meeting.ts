564d55e37b3c18df99ff9f0a023eb51c766696a4
ddenaga <76412992+ddenaga@users.noreply.github.com>
2024-01-31 14:21:48 -0800

    Rename Room to Meeting
    Co-authored-by: Jasjot <jasjot1@users.noreply.github.com>

---
+++ backend/src/Meeting.ts
@@ -0,0 +1,14 @@
+import { Router, Worker } from 'mediasoup/node/lib/types'
+import Peer from './Peer'
+import config from './config'
+
+export default class Meeting extends Router {
+	public peers: Map<string, Peer> | undefined
+
+	public static async build(worker: Worker): Promise<Meeting> {
+		const meeting = (await worker.createRouter(config.routerOptions)) as Meeting
+		meeting.peers = new Map()
+
+		return meeting
+	}
+}
