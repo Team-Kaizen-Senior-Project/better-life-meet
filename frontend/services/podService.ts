c6b0527a13f92af5cc5055e281dfdfa77c891a15
LPZ-Fabian <98778256+LPZ-Fabian@users.noreply.github.com>
2024-02-03 21:46:27 -0800

    KAIZ-86: API Development - Service layer functions for Pod Endpoint (#29)
    * Added service layer functions for Pods and test file in pages
    
    * Fixed comments on test file and added interface for pods when doing PATCH updates
    
    * Remove redundant awaits
    
    ---------
    
    Co-authored-by: ddenaga <76412992+ddenaga@users.noreply.github.com>

---
+++ frontend/services/podService.ts
@@ -0,0 +1,75 @@
+// InterFace for updating pods
+export interface Pod {
+	name?: string
+	meetingTime?: string
+	leader?: string
+	memberId?: string // allows single user updates for pod
+	meetingId?: string // allows single meeting updates for pod
+}
+
+// Fetches a pod from the API using the provided ID
+export async function fetchPod(id: string) {
+	// Construct the URL with query parameters
+	const url = `/api/pod/${id}`
+	const { data, error } = await useFetch(url)
+
+	if (error.value) {
+		console.error('Error fetching pod:', error.value)
+		return null
+	}
+
+	return data.value?.data
+}
+
+// Fetches all pods from the API
+export async function fetchAllPods() {
+	const url = `/api/pod`
+	const { data, error } = await useFetch(url)
+
+	if (error.value) {
+		console.error('Error fetching all pods:', error.value)
+		return null
+	}
+
+	return data.value?.data
+}
+
+// Deletes a pod using the API
+export async function deletePod(id: string) {
+	try {
+		const response = await fetch(`/api/pod/${id}`, {
+			method: 'DELETE',
+		})
+
+		if (!response.ok) {
+			throw new Error('Error deleting Pod')
+		}
+
+		return response.json()
+	} catch (error) {
+		console.error('Error:', error)
+		throw error
+	}
+}
+
+// Updates a pod using the API (allows updates to )
+export async function updatePod(id: string, updateInfo: Pod) {
+	try {
+		const response = await fetch(`/api/pod/${id}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updateInfo),
+		})
+
+		if (!response.ok) {
+			throw new Error('Error updating Pod')
+		}
+
+		return response.json()
+	} catch (error) {
+		console.error('Error:', error)
+		throw error
+	}
+}
