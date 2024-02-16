174a01d584bd30f799a6058d556f5bb5a3da874e
Matt Christiansen <mattchristiansen59@gmail.com>
2024-02-16 14:35:37 -0800

    cleaned

--- tests/unit/components/HabitsList.nuxt.test.ts
+++ tests/unit/components/HabitsList.nuxt.test.ts
@@ -17,6 +17,8 @@ describe('HabitsList', () => {
     it('renders correctly in view mode', async () => {
         const wrapper = await mountSuspended(HabitsList)
         console.log('\x1b[94m%s\x1b[0m', '-'.repeat(100))
+        console.log('\x1b[94m%s\x1b[0m', '-'.repeat(100))
+        console.log('\x1b[94m%s\x1b[0m', '-'.repeat(100))
         console.log(wrapper.text())
         expect(wrapper.text()).toContain('This is habit item number one')
     })
