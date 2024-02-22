333083dbad1eb644d87aa8d35198b4af306c50c9
Matt Christiansen <mattchristiansen59@gmail.com>
2024-02-21 16:23:48 -0800

    add new habit test

--- tests/unit/components/HabitsList.nuxt.test.ts
+++ tests/unit/components/HabitsList.nuxt.test.ts
@@ -56,4 +56,29 @@ describe('HabitsList', () => {
             console.error('Remove button not found')
         }
     })  
+
+    it ('adds a new habit on add button click', async () => {
+        const wrapper = await mountSuspended(HabitsList, {
+            global: {
+                plugins: [createPinia()],
+            },
+        })
+        const initialHabitCount = wrapper.findAll('li').length
+        // trigger edit mode
+        const editButton = wrapper.find('[data-testid="edit-button"]')
+        await editButton.trigger('click')
+        await nextTick()
+    
+        // find and click add button
+        const addButton = wrapper.find('[data-testid="add-habit-button"]')
+        if (addButton.exists()) {
+            await addButton.trigger('click')
+            await nextTick()
+    
+            const updatedHabitCount = wrapper.findAll('li').length
+            expect(updatedHabitCount).toBeGreaterThan(initialHabitCount)
+        } else {
+            console.error('Add button not found')
+        }
+    })
 })
