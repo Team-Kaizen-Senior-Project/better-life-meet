eb0a9f4b495e3b9e019bd4907f35271188eb6936
VictorSeverin <vikea.severin@gmail.com>
2024-02-24 15:24:20 -0800

    countdown component

---
+++ components/MeetingCountdown.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+	const { display: displayDate, dayjs } = useDate()
+	import { useCountdownStore } from '~/stores/CountdownStore'
+	import type { Meeting, Time } from '~/types'
+	const countdownStore = useCountdownStore()
+	const video = useVideoStore()
+	const props = defineProps<{
+		meetingStartTime: Time
+	}>()
+	const countdown = ref('')
+
+	const updateCountdown = () => {
+		const startTime = dayjs(props.meetingStartTime)
+		const now = dayjs()
+		if (startTime.isBefore(now)) {
+			console.log('countdown has finished')
+			countdown.value = '0d 0h 0m 0s'
+			countdownStore.setShowCountdown(false)
+			video.setModalOpen(true)
+			clearInterval(interval) // Stop the countdown
+		} else {
+			const duration = dayjs.duration(startTime.diff(now))
+			countdown.value = `${duration.days()}d ${duration.hours()}h ${duration.minutes()}m ${duration.seconds()}s`
+		}
+	}
+	let interval: any = null
+	onMounted(() => {
+		updateCountdown()
+		interval = setInterval(updateCountdown, 1000)
+	})
+
+	onUnmounted(() => {
+		clearInterval(interval)
+	})
+</script>
+
+<template>
+	<div
+		class="countdown-wrapper flex flex-col items-center justify-center rounded-lg border border-dashed bg-gray-800 px-40 py-20 text-white"
+	>
+		<div class="label text-sm-lg mb-2">Starting in</div>
+		<span class="text-xl font-bold text-white">{{ countdown }}</span>
+	</div>
+</template>
--- pages/meeting/[id]/index.vue
+++ pages/meeting/[id]/index.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 	import { io } from 'socket.io-client'
+	import { useCountdownStore } from '~/stores/CountdownStore'
+	const { display: displayDate, dayjs } = useDate()
 
 	definePageMeta({
 		layout: 'meeting',
 	})
-
+	const countdown = useCountdownStore()
 	const route = useRoute()
 	const meetingId = route.params.id
-
+	// const { getMeeting } = useApi()
+	// const meeting: Meeting = await getMeeting(meetingId)
+	const testingStartTime = '2024-02-24T23:21:00.000Z'
+	const startTime = dayjs(testingStartTime)
+	const now = dayjs()
+	if (now.isBefore(startTime)) {
+		countdown.setShowCountdown(true)
+		console.log('before')
+	}
+	console.log('Now', now)
+	console.log('Start Time', startTime)
 	// Connect to websocket server
 	const ws = io()
 </script>
@@ -15,7 +27,8 @@
 <template>
 	<PodHeader />
 	<div class="flex min-h-[82vh] items-center justify-center bg-zinc-800">
-		<div class="grid h-[70vh] w-[80vw] grid-cols-4 grid-rows-2 gap-3">
+		<MeetingCountdown v-if="countdown.showCountdown" :meetingStartTime="testingStartTime" />
+		<div v-else class="grid h-[70vh] w-[80vw] grid-cols-4 grid-rows-2 gap-3">
 			<!-- Local user's video feed -->
 			<div class="relative overflow-hidden rounded-lg bg-zinc-900" v-if="true">
 				<LocalVideo />
