fa2f36d0161582cae9b2d6229567b2cbe322545d
sparshs916 <sparshs916@gmail.com>
2024-03-17 18:31:04 -0700

    Add slash icons in footer video toggles

--- components/PodFooter.vue
+++ components/PodFooter.vue
@@ -14,11 +14,17 @@ const { isLocalAudioEnabled, isLocalVideoEnabled, toggleAudio, toggleVideo } = g
 				<div class="flex gap-2">
 					<Button class="rounded px-5 py-3" @click="toggleAudio">
 						<MicrophoneIcon v-if="isLocalAudioEnabled" class="h-5 w-5 text-green-500" />
-						<MicrophoneIcon v-else class="h-5 w-5 text-red-500" />
+						<svg v-else class="h-5 w-5 text-red-500" xmlns='http://www.w3.org/2000/svg' fill='none'
+							strokeWidth={1.5} viewBox='0 0 24 24' stroke='currentColor' className='w-6 h-6'>
+							<path strokeLinecap='round' strokeLinejoin='round'
+								d='M19 19L17.591 17.591L5.409 5.409L4 4' />
+							<path strokeLinecap='round' strokeLinejoin='round'
+								d='M12 18.75C13.5913 18.75 15.1174 18.1179 16.2426 16.9926C17.3679 15.8674 18 14.3413 18 12.75V11.25M12 18.75C10.4087 18.75 8.88258 18.1179 7.75736 16.9926C6.63214 15.8674 6 14.3413 6 12.75V11.25M12 18.75V22.5M8.25 22.5H15.75M12 15.75C11.2044 15.75 10.4413 15.4339 9.87868 14.8713C9.31607 14.3087 9 13.5456 9 12.75V4.5C9 3.70435 9.31607 2.94129 9.87868 2.37868C10.4413 1.81607 11.2044 1.5 12 1.5C12.7956 1.5 13.5587 1.81607 14.1213 2.37868C14.6839 2.94129 15 3.70435 15 4.5V12.75C15 13.5456 14.6839 14.3087 14.1213 14.8713C13.5587 15.4339 12.7956 15.75 12 15.75Z' />
+						</svg>
 					</Button>
 					<Button class="rounded px-5 py-3" @click="toggleVideo">
 						<VideoCameraIcon v-if="isLocalVideoEnabled" class="h-5 w-5 text-green-500" />
-						<VideoCameraIcon v-else class="h-5 w-5 text-red-500" />
+						<VideoCameraSlashIcon v-else class="h-5 w-5 text-red-500" />
 					</Button>
 					<Button class="rounded px-5 py-3">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
