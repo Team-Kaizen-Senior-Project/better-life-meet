14de03001e7d7341d4a922a4b313b4d649e5aa4d
Matt Christiansen <mattchristiansen59@gmail.com>
2024-03-30 09:42:53 -0700

    updated prerecorded video tests and added test, added test ids in dashboard

--- tests/unit/components/PrerecordedVideo.nuxt.test.ts
+++ tests/unit/components/PrerecordedVideo.nuxt.test.ts
@@ -2,9 +2,14 @@ import { describe, it } from 'vitest'
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 import PrerecordedVideo from '@/components/PrerecordedVideo.vue'
 
-describe('PrerecordedVideo.vue', () => {
+describe('PrerecordedVideo', () => {
 	it('renders the video element with the correct attributes', async () => {
-		const wrapper = await mountSuspended(PrerecordedVideo)
+		const wrapper = await mountSuspended(PrerecordedVideo, {
+			props: {
+				meetingStartTime: { isoString: new Date().toISOString() }
+			}
+		})
+
 		const video = wrapper.find('video')
 		expect(video.exists()).toBe(true)
 		expect(video.attributes('controls')).toBeDefined()
@@ -13,9 +18,26 @@ describe('PrerecordedVideo.vue', () => {
 	})
 
 	it('emits "toggleVideo" when the video has ended', async () => {
-		const wrapper = await mountSuspended(PrerecordedVideo)
+		const wrapper = await mountSuspended(PrerecordedVideo, {
+			props: {
+				meetingStartTime: { isoString: new Date().toISOString() }
+			}
+		})
 		const video = wrapper.find('video')
 		await video.trigger('ended')
 		expect(wrapper.emitted('toggleVideo')).toBeTruthy()
+	})
+
+	it ('emits toggleVideo event when user is too late to watch the video', async () => {
+		const wrapper = await mountSuspended(PrerecordedVideo, {
+			props: {
+				meetingStartTime: { isoString: new Date(Date.now() - 1000 * 60 * 60 * 24).toISOString() } // 24 hours ago
+			}
+		})
+
+		const video = wrapper.find('video')
+		await video.trigger('ended')
+		await wrapper.vm.$nextTick()
+		expect(wrapper.emitted().toggleVideo).toBeTruthy()
 	})
 })
