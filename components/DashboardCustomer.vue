89a60f588ebc1cdc8c942470b63b738f4b53d2dc
Brian <bdlowery2@gmail.com>
2024-04-02 18:27:20 -0600

    merge main

--- components/DashboardCustomer.vue
+++ components/DashboardCustomer.vue
@@ -1,24 +1,10 @@
 <script setup lang="ts">
-	import MeetingCard from '~/components/MeetingCard.vue'
-	import type { Meeting } from '~/types'
-	import type {} from '@samk-dev/nuxt-vcalendar'
+	import MeetingCard from '@/components/MeetingCard.vue'
+	import MeetingCalendar from '@/components/MeetingCalendar.vue'
 
 	const { getMeetings } = useApi()
 
 	const { state: podState } = usePodStore()
-
-	const date = ref(new Date())
-
-	const attrs = ref<any>([
-		{
-			key: 'today',
-			highlight: {
-				color: 'green',
-				fillMode: 'solid',
-			},
-			dates: new Date(),
-		},
-	])
 
 	const { data, refresh, pending } = await useAsyncData('dashboard', async () => {
 		const [meetings] = await Promise.all([
@@ -56,19 +42,15 @@
 				<!-- column 3 -->
 				<div class="col-span-1 grid gap-4 self-start lg:col-span-3">
 					<div class="rounded-lg bg-zinc-900 p-4">
-						<h2 class="mb-4 text-lg font-medium text-white">Upcoming events</h2>
-						<ClientOnly>
-							<div class="my-calendar">
-								<VCalendar v-model="date" :attributes="attrs" class="" isDark />
-							</div>
-						</ClientOnly>
+						<h2 class="mb-4 text-lg font-medium text-white" data-testid="upcoming-events">Upcoming events</h2>
+						<MeetingCalendar :meetings="data?.meetings" />
 						<div class="mt-4">
 							<ScheduleMeetingCard @refresh="refresh" />
 						</div>
 					</div>
 
 					<div class="rounded-lg bg-zinc-900 p-4">
-						<h2 class="mb-4 text-lg font-medium text-white">Meetings this week</h2>
+						<h2 class="mb-4 text-lg font-medium text-white" data-testid="meetings-this-week">Meetings this week</h2>
 						<div class="grid grid-cols-1 gap-4">
 							<div v-if="data?.meetings?.length" v-for="(meeting, index) in data?.meetings" class="">
 								<MeetingCard :meeting="meeting" :isFirst="index === 0" @refresh="refresh" />
@@ -83,21 +65,12 @@
 		</inner-column>
 	</section>
 </template>
-
 <style scoped>
 	.count {
 		display: flex;
 		gap: 10px;
 		align-items: flex-start;
 	}
-
-	.my-calendar :deep(.vc-dark) {
-		--vc-bg: #27272a;
-	}
-	.my-calendar :deep(.vc-bordered) {
-		border-color: transparent;
-	}
-
 	h3 {
 		margin: 0;
 	}
