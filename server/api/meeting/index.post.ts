da0ccf7eca5729163dcc8163f80b2f45ba744b24
jasjot1 <sjasjot123@gmail.com>
2024-04-12 15:12:02 -0700

    Update 100ms meeting name to include pod id

--- server/api/meeting/index.post.ts
+++ server/api/meeting/index.post.ts
@@ -34,7 +34,7 @@ export default defineEventHandler(async (event) => {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
-				name: `Meeting ${meeting.startTime}`,
+				name: `Meeting ${meeting.startTime} Pod ${meeting.podRef}`,
 				description: 'Meeting room',
 			}),
 		})
